d3fb232a759155fc005f8d43dfdacb07262cec02
Park Sung Min <vamf12@gmail.com>
2017-08-09 14:12:37 +0900

    타입형 변황

--- src/main/kotlin/com/minek/kotlin/everywhere/keduct/bluebird/bluebird.kt
+++ src/main/kotlin/com/minek/kotlin/everywhere/keduct/bluebird/bluebird.kt
@@ -2,7 +2,7 @@ package com.minek.kotlin.everywhere.keduct.bluebird
 
 
 @JsModule("bluebird")
-external class Bluebird<T>(body: (resolve: (T) -> Unit, reject: (e: Exception) -> Unit) -> Unit) {
+external class Bluebird<out T>(body: (resolve: (T) -> Unit, reject: (e: Exception) -> Unit) -> Unit) {
     fun <U : Any> then(body: (T) -> U): Bluebird<U>
     @JsName("then")
     fun <U : Bluebird<V>, V : Any> andThen(body: (T) -> U): Bluebird<V>
